8329326b3889e4dd8fac7ed7c195ab57474942c4
bhankymyk <bankole123michael@gmail.com>
2025-02-16 22:18:30 +0100

    feat: ui design

--- src/views/MathsView.vue
+++ src/views/MathsView.vue
@@ -1,114 +1,128 @@
 <template>
-  <div class="max-w-lg mx-auto p-6 bg-white rounded-3xl">
-    <div class="flex items-center justify-between mb-8">
-      <div class="rounded-lg border border-gray-400 pt-1 px-1">
-        <button @click="goBack" class="">
-          <img src="/src/assets/arrow-back-2.png" class="w-5 h-5 text-green-700" alt="back" />
+  <div class="max-w-lg mx-auto bg-gray-100 rounded-3xl">
+    <div class="p-6">
+      <div class="flex items-center justify-between mb-8">
+        <div class="rounded-lg border border-gray-400 pt-1 px-1">
+          <button @click="goBack" class="">
+            <img src="/src/assets/arrow-back-2.png" class="w-5 h-5 text-green-700" alt="back" />
+          </button>
+        </div>
+        <h1 class="text-xl font-semibold">Course Preview</h1>
+        <button class="rounded-lg border border-gray-400 py-1 px-1">
+          <img src="/src/assets/question-mark.png" class="w-5 h-5" />
         </button>
       </div>
-      <h1 class="text-xl font-semibold">Course Preview</h1>
-      <button class="rounded-lg border border-gray-400 py-1 px-1">
-        <img src="/src/assets/question-mark.png" class="w-5 h-5" />
-      </button>
-    </div>
 
-    <div class="flex justify-between">
-      <div class="">
-        <div class="text-sm mb-2">Lesson 1.3</div>
-        <div class="flex gap-1">
-          <div class="h-1 w-7 bg-purple-600 rounded"></div>
-          <div class="h-1 w-7 bg-purple-200 rounded"></div>
-          <div class="h-1 w-7 bg-purple-200 rounded"></div>
-          <div class="h-1 w-7 bg-purple-200 rounded"></div>
+      <div class="flex justify-between">
+        <div class="">
+          <div class="text-sm mb-2">Lesson 1.3</div>
+          <div class="flex gap-1">
+            <div class="h-1 w-7 bg-purple-600 rounded"></div>
+            <div class="h-1 w-7 bg-purple-200 rounded"></div>
+            <div class="h-1 w-7 bg-purple-200 rounded"></div>
+            <div class="h-1 w-7 bg-purple-200 rounded"></div>
+          </div>
+        </div>
+        <div
+          class="flex justify-between items-center border bg-purple-50 rounded-full p-2 space-x-2"
+        >
+          <img src="/src/assets/clock.png" class="w-5 h-5" />
+          <p class="text-purple-600 text-base font-bold">{{ formattedTime }}</p>
         </div>
       </div>
-      <div class="flex justify-between items-center border bg-purple-50 rounded-full p-2 space-x-2">
-        <img src="/src/assets/clock.png" class="w-5 h-5" />
-        <p class="text-purple-600 text-base font-bold">{{ formattedTime }}</p>
-      </div>
-    </div>
 
-    <h2 class="text-xl font-semibold my-6">Match the Algebraic Terms!</h2>
+      <h2 class="text-xl font-semibold my-6">Match the Algebraic Terms!</h2>
 
-    <div class="grid gap-4 mb-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-      <div
-        v-for="definition in items"
-        :key="definition.id"
-        class="p-4 rounded-lg transition-colors duration-200 border border-dashed relative"
-        :class="{
-          'bg-yellow-100': definition.matched,
-          'bg-purple-50 border-purple-300 opacity-50':
-            !definition.matched && !(isDraggingOver === definition.id),
-          'bg-red-100':
-            !definition.matched &&
-            isDraggingOver === definition.id &&
-            selectedAnswer !== definition.id,
-          'bg-green-100': definition.matched && definition.isCorrect,
-        }"
-        @dragover.prevent="handleDragOver($event, definition)"
-        @dragleave="handleDragLeave"
-        @drop="handleDrop($event, definition)"
-      >
-        {{ definition.text }}
-        <div v-if="definition.matched" class="absolute top-2 right-2">
-          <svg
-            v-if="definition.isCorrect"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 text-green-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path fill-rule="evenodd" d="M5 10l4 4L15 6" clip-rule="evenodd" />
-          </svg>
-          <svg
-            v-else-if="definition.isWrong"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 text-red-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path fill-rule="evenodd" d="M5 5l10 10M5 15L15 5" clip-rule="evenodd" />
-          </svg>
+      <div class="grid gap-4 mb-8 grid-cols-2">
+        <div
+          v-for="(definition, index) in items"
+          :key="definition.id"
+          class="p-4 rounded-lg transition-colors duration-200 border border-dashed relative"
+          :class="[
+            {
+              'bg-yellow-100': definition.matched,
+              'bg-purple-50 border-purple-300 opacity-50':
+                !definition.matched && !(isDraggingOver === definition.id),
+              'bg-red-100':
+                (!definition.matched &&
+                  isDraggingOver === definition.id &&
+                  selectedAnswer !== definition.id) ||
+                definition.isWrong,
+              'bg-green-100': definition.matched && definition.isCorrect,
+            },
+            index === items.length - 1 && items.length % 2 === 1 ? 'col-span-2 w-1/2 mx-auto' : '',
+          ]"
+          @dragover.prevent="handleDragOver($event, definition)"
+          @dragleave="handleDragLeave"
+          @drop="handleDrop($event, definition)"
+        >
+          {{ definition.text }}
+          <div v-if="definition.matched" class="absolute top-6 right-2">
+            <div class="" v-if="definition.isCorrect">
+              <img src="/src/assets/checked.png" class="w-5 h-5" />
+            </div>
+
+            <div class="div" v-if="definition.isWrong">
+              <img src="/src/assets/checkedwrong.png" class="w-5 h-5" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="grid grid-cols-2 gap-4 mb-8 sm:grid-cols-3 lg:grid-cols-4">
-      <div
-        v-for="item in items"
-        :key="item.id"
-        draggable="true"
-        @dragstart="handleDragStart($event, item)"
-        @dragend="handleDragEnd"
-        class="py-6 bg-gray-800 text-white rounded-xl text-center cursor-move transition-opacity duration-200"
-        :class="{ 'opacity-10 text-opacity-50': item.matched }"
-      >
-        {{ capitalizeFirst(item.id) }}
+    <div class="bg-white p-6 mx-1 rounded-3xl">
+      <h5 class="text-center">
+        Drag the correct Algebraic term below to match the <br />
+        definition above.
+      </h5>
+      <div class="grid grid-cols-2 gap-4 mb-8 mt-5">
+        <div
+          v-for="(item, index) in items"
+          :key="item.id"
+          draggable="true"
+          @dragstart="handleDragStart($event, item)"
+          @dragend="handleDragEnd"
+          class="py-6 bg-gray-800 text-white rounded-xl text-center cursor-move transition-opacity duration-200"
+          :class="[
+            { 'opacity-10 text-opacity-50': item.matched },
+            index === items.length - 1 && items.length % 2 === 1 ? 'col-span-2 w-1/2 mx-auto' : '',
+          ]"
+        >
+          {{ capitalizeFirst(item.id) }}
+        </div>
+      </div>
+
+      <div class="flex justify-between">
+        <div class="flex items-center rounded-xl p-3 px-5 border border-gray-400 mr-10">
+          <button @click="reloadGame">
+            <img src="/src/assets/refresh.svg" class="w-8 h-8 text-purple-800" alt="reload" />
+          </button>
+        </div>
+
+        <button
+          class="w-full bg-purple-800 text-white py-4 rounded-xl flex items-center justify-center transition-colors hover:bg-purple-700"
+          :disabled="!allMatched"
+          :class="{ 'opacity-50 cursor-not-allowed': !allMatched }"
+          @click="handleContinueClick"
+        >
+          Continue
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 ml-2"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+
+        <!-- <div v-if="allMatched" class="">All items matched!</div> -->
       </div>
     </div>
-
-    <button
-      class="w-full bg-purple-800 text-white py-4 rounded-xl flex items-center justify-center transition-colors hover:bg-purple-700"
-      :disabled="!allMatched"
-      :class="{ 'opacity-50 cursor-not-allowed': !allMatched }"
-      @click="handleContinueClick"
-    >
-      Continue
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 ml-2"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </button>
-
-    <div v-if="allMatched" class="text-center text-green-500">All items matched!</div>
   </div>
 </template>
 
@@ -124,14 +138,40 @@ interface MatchingItem {
   isCorrect?: boolean
   isWrong?: boolean
   droppedItems?: MatchingItem[]
+  originalText?: string
 }
 
 const items = ref<MatchingItem[]>([
-  { id: 'variable', text: 'A letter or symbol that represents a value', matched: false },
-  { id: 'constant', text: 'A fixed number that does not change', matched: false },
-  { id: 'coefficient', text: 'A number that multiplies a variable', matched: false },
-  { id: 'expression', text: 'A combination of numbers, variables, and operations', matched: false },
-  { id: 'equation', text: 'A statement that two expressions are equal', matched: false },
+  {
+    id: 'variable',
+    text: 'A letter or symbol that represents a value',
+    originalText: 'A letter or symbol that represents a value',
+    matched: false,
+  },
+  {
+    id: 'constant',
+    text: 'A fixed number that does not change',
+    originalText: 'A fixed number that does not change',
+    matched: false,
+  },
+  {
+    id: 'coefficient',
+    text: 'A number that multiplies a variable',
+    originalText: 'A number that multiplies a variable',
+    matched: false,
+  },
+  {
+    id: 'expression',
+    text: 'A combination of numbers, variables, and operations',
+    originalText: 'A combination of numbers, variables, and operations',
+    matched: false,
+  },
+  {
+    id: 'equation',
+    text: 'A statement that two expressions are equal',
+    originalText: 'A statement that two expressions are equal',
+    matched: false,
+  },
 ])
 
 const router = useRouter()
@@ -142,7 +182,14 @@ const handleContinueClick = () => {
   }
 }
 
-const timeLeft = ref(60)
+const itemsMatched = () => {
+  toast('All items matched!', {
+    autoClose: 1000,
+    position: toast.POSITION.TOP_CENTER,
+  } as ToastOptions)
+}
+
+const timeLeft = ref(120)
 const timer = ref<ReturnType<typeof setInterval> | null>(null)
 
 const formattedTime = computed(() => {
@@ -169,6 +216,7 @@ const handleTimeUp = () => {
     autoClose: 1000,
     position: toast.POSITION.TOP_CENTER,
   } as ToastOptions)
+  window.location.reload()
 }
 
 onMounted(() => {
@@ -180,6 +228,14 @@ onUnmounted(() => {
     clearInterval(timer.value)
   }
 })
+
+// const itemsMatched = () => {
+//   toast('All items matched!', {
+//     autoClose: 1000,
+//     position: toast.POSITION.TOP_CENTER,
+//   } as ToastOptions)
+// }
+
 const allMatched = computed(() => items.value.every((item) => item.matched))
 const selectedAnswer = ref<string | null>(null)
 const isDraggingOver = ref<string | null>(null)
@@ -222,14 +278,22 @@ const handleDrop = (event: DragEvent, definition: MatchingItem) => {
         droppedItem.matched = true
         droppedItem.isCorrect = true
         droppedItem.isWrong = false
+
+        definition.text = capitalizeFirst(itemId)
       } else {
         droppedItem.matched = false
         droppedItem.isCorrect = false
         droppedItem.isWrong = true
+
+        definition.text = capitalizeFirst(itemId)
       }
 
       definition.droppedItems = definition.droppedItems || []
       definition.droppedItems.push(droppedItem)
+
+      if (allMatched.value) {
+        itemsMatched()
+      }
     }
   }
 
@@ -242,6 +306,24 @@ const capitalizeFirst = (str: string): string => {
 
 const goBack = () => {
   router.back()
+}
+
+const reloadGame = () => {
+  items.value = items.value.map((item) => ({
+    ...item,
+    text: item.originalText || item.text,
+    matched: false,
+    isCorrect: false,
+    isWrong: false,
+    droppedItems: undefined,
+  }))
+
+  timeLeft.value = 120
+
+  if (timer.value) {
+    clearInterval(timer.value)
+  }
+  startTimer()
 }
 </script>
 
@@ -273,7 +355,7 @@ const goBack = () => {
 }
 
 @media screen and (min-width: 375px) {
-  .grid {
+  .grido {
     grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
   }
 }
