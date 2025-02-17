e95377f0a6fff3d43810d87be55dd39d8d6bfc09
bhankymyk <bankole123michael@gmail.com>
2025-02-17 04:48:35 +0100

    fix:  build

--- src/views/SuccessView.vue
+++ src/views/SuccessView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-auto bg-gray-100 rounded-3xl p-6 mt-1 items-center">
-    <h2 class="text-3xl font-semibold text-center">
+    <h2 class="lg:text-3xl font-semibold text-center">
       Phew!!! <br />
       You make it this far, that is a huge work on your end, <br />
       even though you might not be satisfied with your score there is a room to do better, you are a
